21b84f2485980e4a5a082d8f68d7bc4a62d9a58b
Mina Cheng <mina891015@gmail.com>
2021-07-04 23:44:53 +0800

    Finish basic styles and layouts bootstrap

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, beatae. Similique nisi perferendis illum consequuntur laboriosam omnis nostrum aliquid ipsum laudantium. Vero, ad quod! Culpa fugiat temporibus numquam quae maiores.",
+            price: price
         })
         await camp.save();
     }
